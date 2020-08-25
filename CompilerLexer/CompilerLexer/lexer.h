4d3632b855ff8b3dd3334a08181238830cbe3665
Daniel Atonge <elamboatonge@gmail.com>
2020-08-25 17:27:31 +0300

    Update lexer.h

--- CompilerLexer/CompilerLexer/lexer.h
+++ CompilerLexer/CompilerLexer/lexer.h
@@ -1,11 +1,3 @@
-//
-//  lexer.h
-//  CompilerLexer
-//
-//  Created by MacBookPro on 23.08.2020.
-//  Copyright © 2020 MacBookPro. All rights reserved.
-//
-
 #ifndef lexer_h
 #define lexer_h
 
@@ -27,10 +19,11 @@ void lexer_advance(lexer_t* lexer);
 void lexer_stepback(lexer_t* lexer);
 void lexer_skip_whitespace(lexer_t* lexer);
 token_t* lexer_get_next_token(lexer_t* lexer);
-token_t* lexer_collect_string(lexer_t* lexer);
+token_t* lexer_build_string(lexer_t* lexer);
 token_t* lexer_collect_value(lexer_t* lexer);
-token_t* lexer_collect_id(lexer_t* lexer);
-char* lexer_get_current_char_as_string(lexer_t* lexer, token_t* token);
+token_t* lexer_collect_identifier(lexer_t* lexer);
+char* lexer_get_current_char(lexer_t* lexer, token_t* token);
 token_t* lexer_advance_with_token(lexer_t* lexer, token_t* token);
+void lexer_remove_comment(lexer_t* lexer);
 
 #endif /* lexer_h */
