dab82b946e984eb3144af488dbb250834b4b9cd9
Daniel Atonge <elamboatonge@gmail.com>
2020-08-25 22:47:20 +0300

    Update main.cpp

--- CompilerLexer/CompilerLexer/main.cpp
+++ CompilerLexer/CompilerLexer/main.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "lexer.h"
 
+/* Returns token name as a string*/
 const char* get_category(type ntype){
     switch (ntype) {
         case IDENTIFIER: return "IDENTIFIER";
@@ -26,13 +27,13 @@ int main(int argc, const char * argv[]) {
     char* content = (char*)calloc(1, sizeof(char));
     content[0] = '\0';
     unsigned position = 0;
-    FILE *fp = fopen("test.txt", "r");
     
+    /* Opening and reading from source language file */
+    FILE *fp = fopen("test.txt", "r");
     if (fp == nullptr) {
         perror("Error while opening the file.\n");
         exit(EXIT_FAILURE);
     }
-    
     while((ch = fgetc(fp)) != EOF) {
         content = (char*) realloc(content, (strlen(content) + 1)* sizeof(char));
         content[position++] = ch;
@@ -41,9 +42,9 @@ int main(int argc, const char * argv[]) {
     fclose(fp);
     
     lexer_t* lexer = init_lexer(content);
-                    
     token_t* token = nullptr;
     
+    /* Get and print tokens */
     while((token = lexer_get_next_token(lexer)) != nullptr)
     {
         printf("<%s, %s, %d, %d>\n", get_category(token->type), token->value, token->line_number, token->col_number);
