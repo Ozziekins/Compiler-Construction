a4bdd86c5f6ac13fbe9633c4e4b87047ac5269ea
Daniel Atonge <elamboatonge@gmail.com>
2020-08-25 22:47:43 +0300

    Update token.cpp

--- CompilerLexer/CompilerLexer/token.cpp
+++ CompilerLexer/CompilerLexer/token.cpp
@@ -1,15 +1,8 @@
-//
-//  token.cpp
-//  CompilerLexer
-//
-//  Created by MacBookPro on 23.08.2020.
-//  Copyright © 2020 MacBookPro. All rights reserved.
-//
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "token.h"
 
+/* initializes token with its type, value and location */
 token_t* init_token(type type, char* value, unsigned line, unsigned col){
     token_t* token = (token_t* )calloc(1, sizeof(struct TOKEN_STRUCT));
     token->type = type;
