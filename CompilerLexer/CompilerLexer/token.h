51bde751f850531d6c2a0660be04bef8deec1f69
Daniel Atonge <elamboatonge@gmail.com>
2020-08-25 22:48:09 +0300

    Update token.h

--- CompilerLexer/CompilerLexer/token.h
+++ CompilerLexer/CompilerLexer/token.h
@@ -1,21 +1,23 @@
 #ifndef token_h
 #define token_h
 
+/* lists of possible tokens */
 typedef enum {
-    IDENTIFIER,
-    KEYWORD,
-    LITERAL,
-    SEMI,
-    LPAREN,
-    RPAREN,
-    LSQUARE,
-    RSQUARE,
-    LCURLY,
-    RCURLY,
-    OPERATOR,
-    COMMA
+    IDENTIFIER,             // variable names e.g foo
+    KEYWORD,                // source language specific identifiers e.g for
+    LITERAL,                // constants or strings e.g "good", 40
+    SEMI,                   // end of statement character (;)
+    LPAREN,                 // (
+    RPAREN,                 // )
+    LSQUARE,                // [
+    RSQUARE,                // ]
+    LCURLY,                 // {
+    RCURLY,                 // }
+    OPERATOR,               // all operators present in source language description e.g >
+    COMMA                   // ,
 } type;
 
+/* Token structure consisting of the token's type, value and location */
 typedef struct TOKEN_STRUCT
 {
     type type;
@@ -24,6 +26,7 @@ typedef struct TOKEN_STRUCT
     unsigned int col_number;
 } token_t;
 
+/* initializes token with its type, value and location */
 token_t* init_token(type type, char* value, unsigned line, unsigned col);
 
 #endif /* token_h */
