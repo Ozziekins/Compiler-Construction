de53ea9b6d8d1448a3d78d3c48dd3cecd7843a30
elamboatonge <elamboatonge@gmail.com>
2020-09-11 20:31:37 +0300

    no lexer.cpp

--- LexerGrammar/source_code.d
+++ LexerGrammar/source_code.d
@@ -1,10 +1,1 @@
-var factorial := func (n) is 
-    if n = 0 then		// Brackets are omitted intentionally
-    	return 1;
-    else
-        return n * factorial(n - 1);
-    end
-end;
-var lambdafunc := func( x ) => x/1.4;
-
-print  lambdafunc( factorial( 4 ) ) , " The output should be 24/1.4 ~= 17,142";
+var name;
