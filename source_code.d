dd011c8d8810a22e707ea3bd5db93b22b2b8a17f
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2020-10-10 02:17:17 +0300

    Amazing pool request [ Thanks to Pavel ]

--- source_code.d
+++ source_code.d
@@ -1,14 +1,25 @@
-var c;
-c := func (x, y, z) is 
-  var count := 0;
-    while x >= y loop
-        if y > z then
-            y := y + 1;
-        else
-            z := z - 1;
-        end
-    count := count + 1;
-    end
-    return count;
- end;
- print c(10, 5, 7);
+var this := 7;
+var that := 17.9;
+var those := "anything";
+var e;
+
+var r := this is int;    //TRUE
+var s := that is real;      //TRUE
+var t := those is string;  //TRUE
+var u := e is empty;        //TRUE
+
+//var sas := empty;
+//var syas := empty;
+//print sas + syas;	//ERROR
+
+print r;
+print s;
+print t;
+print u;
+
+var a := true and true;
+var b := not true;
+var c := false xor false;
+var d := true or true;
+
+print "if " , a , " then not " , b , " but " + " if " , c , " then " , d;
